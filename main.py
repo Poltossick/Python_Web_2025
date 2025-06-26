5656eb6e6f68af264050c8b24e6d87ab5a7f6743
Poltossick <1812.lev@gmail.com>
2025-06-26 16:15:57 +0300

    Задача №2

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-for i in range(1, 10):
-    for j in range(1, 10):
-        print(f'{i}*{j} = {i * j}', end='\t')
-    print()
+min_val = float('inf')
+max_val = float('-inf')
+total = 0
+total_suc = 0
+
+while (num := int(input('Введите рост участника: '))) != -1:
+    if 150 <= num <= 180:
+        total_suc += 1
+        if num < min_val:
+            min_val = num
+        if num > max_val:
+            max_val = num
+    total += 1
+
+print(f'Итого\n\tвсего = {total}\n\tпрошли успешно = {total_suc}'
+f'\n\tминимальный рост = {min_val}\n\tмаксимальный рост = {max_val}')
