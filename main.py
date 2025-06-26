d9006b52f0482745d929e3092c57a3ab120f0dea
Poltossick <1812.lev@gmail.com>
2025-06-26 11:52:14 +0300

    While (Начало цикла / Loop Begin)

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-print('ax + bx + c = 0')
-a = int(input('Введите a: '))
-b = int(input('Введите b: '))
-c = int(input('Введите c: '))
-if a != 0:
-    d = b ** 2 - 4 * a * c
-    if d < 0:
-        print('d =', (b ** 2 - 4 * a * c))
-        print('Уравнение не имеет корней')
-    elif d == 0:
-        print('d =', (b ** 2 - 4 * a * c))
-        print(f'Корень уравнения:\n\tx =', - b / 2 * a)
-    elif d > 0:
-        print('d =', (b ** 2 - 4 * a * c))
-        print(f'Корни уравнениия:\n\tx1 =', round((- b + d ** (1 / 2)) / 2 * a, 2),
-        f'\n\tx2 =', round((- b - d ** (1 / 2)) / 2 * a, 2))
-else:
-        print('"a" не может быт меньше нуля')
+counter = 0
+while counter < 5:
+    print(f'Иттерация {counter + 1}')
+    counter += 1
+print(f'Итого в counter уже {counter}')
+print('Обратный отсчет: ')
+while counter >= 0:
+    print(f'Иттерация {counter}')
+    counter -= 1
