251bf7362135fdbe14a40e7d28b0ce5a1130b1a6
Poltossick <1812.lev@gmail.com>
2025-06-26 14:35:46 +0300

    Select numbers

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-print(f'Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо\n\tQ - выход')
-flag = True
-while flag:
-    ch = input('Ваш выбор: ')
-    match ch:
-        case 'L' | 'l' | 'д' | 'Д' :
-            print('Свернули налево')
-        case 'R' :
-            print('Свернули направо')
-        case 'F':
-            print('Пошли прямо')
-        case 'Q' | 'q' | 'Й' | 'й' :
-            print('выход в меню')
-            flag = False
-        case _:
-            print('выбор не ясен')
+counter = 1
+while counter <= 100:
+    if counter % 10 == 3:
+        print(counter)
+    counter += 1
