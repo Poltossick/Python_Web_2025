44196f467c717bf4f0916451026aea6a23917fc0
Poltossick <1812.lev@gmail.com>
2025-06-26 15:50:03 +0300

    Вложенные циклы

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-N = 5
-total = 0
-min_val = float('inf')  # + бесконечность
-max_val = float('-inf')  # - бесконечность
-prod = 1
-for _ in range(N):
-    num = int(input('Введите целое число: '))
-    if num < min_val:
-        min_val = num
-    if num > max_val:
-        max_val = num
-    total += num
-    ave = total / N
-    prod *= num
-print('Сумма =', total)
-print('Среднее арифметическое =', ave)
-print('минимум =', min_val)
-print('максимум =', max_val)
-print('произведение =', prod)
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(f'{i}*{j} = {i * j}', end='\t')
+    print()
