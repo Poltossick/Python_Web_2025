33d6681a99d9ae1bc9f018d6506d6e8ae2891f8e
Poltossick <1812.lev@gmail.com>
2025-06-26 15:39:12 +0300

    Аргументы

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-# word = 'поток'
-# for ch in word:
-#     print(ch)
-
-for i in range(1,101):
-    if i % 10 == 5:
-        print(i)
+N = 5
+total = 0
+min_val = float('inf')  # + бесконечность
+max_val = float('-inf')  # - бесконечность
+prod = 1
+for _ in range(N):
+    num = int(input('Введите целое число: '))
+    if num < min_val:
+        min_val = num
+    if num > max_val:
+        max_val = num
+    total += num
+    ave = total / N
+    prod *= num
+print('Сумма =', total)
+print('Среднее арифметическое =', ave)
+print('минимум =', min_val)
+print('максимум =', max_val)
+print('произведение =', prod)
