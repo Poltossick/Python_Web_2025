4d95bdb32dc0a1d526636e7710fe8168a06bae7f
Poltossick <1812.lev@gmail.com>
2025-06-27 15:42:48 +0300

    encrypting 1

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-# word = 'пароль'
-# # item = ch = i
-# v = 0 # число гласных изначально
-# vowels = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
-# for item in word: # перебрать по элементам
-#     if item in vowels:
-#         v += 1
-# print('гласных в строке =', v)
-#
-# for index in range(len(word)):
-#     print(word[index])
-
-# # ИСПРАВИТЬ БУКВУ В СЛОВЕ
-# word = 'малоко'
-# res = ''
-# for ch in word:
-#     if ch == 'а':
-#         res += 'о'
-#     else:
-#         res += ch
-# print(res)
-
-u = '\u0176'
-d = '\u0186'
-print(u, d)
-
+word = input('введите слово для зашифровки: ')
+s = set()
+res = ''
+for ch in word:
+    s.add(ord(ch))
+print(s)
+for item in s:
+    res += chr(item)
+print(res)
