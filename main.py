7923a145c1118626148dd443988c046dc77268f0
Poltossick <1812.lev@gmail.com>
2025-06-27 15:29:08 +0300

    Str vowels

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+# word = 'пароль'
+# # item = ch = i
+# v = 0 # число гласных изначально
+# vowels = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
+# for item in word: # перебрать по элементам
+#     if item in vowels:
+#         v += 1
+# print('гласных в строке =', v)
+#
+# for index in range(len(word)):
+#     print(word[index])
 
-koin1 = {'орел', 'решка'}
-koin2 = {'орел', 'решка'}
-side1 = koin1.pop()
-side2 = koin2.pop()
-flag = True
-print(side1 != side2)
+# # ИСПРАВИТЬ БУКВУ В СЛОВЕ
+# word = 'малоко'
+# res = ''
+# for ch in word:
+#     if ch == 'а':
+#         res += 'о'
+#     else:
+#         res += ch
+# print(res)
+
+u = '\u0176'
+d = '\u0186'
+print(u, d)
+
