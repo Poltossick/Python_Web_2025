4a448c28f972d22d25aeba5f430bcd894751a45f
Poltossick <1812.lev@gmail.com>
2025-06-30 16:17:35 +0300

    Sorted // enumerate //

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-#
-# channels = ['red', 'greed', 'blue'] # == ('red', 'greed', 'blue')
-# # r, *g = channels # распаковка
-# # количество элементов должно быть равно количеству переменных, если меньше, написать ' * ' перед последним
-# a, b, c = channels # распаковка
-# print(b)
+# функция sorted() - на выходе получает сортированный список
+# s = {'Петров', 'Иванов', 'Сидоров'}
+# """
+# lst = list(s)
+# lts.sort()
+# print(*lst, sep=', ')
+# """
+# name = sorted({'Петров', 'Иванов', 'Сидоров'}, reverse=True)
+# print(*name, sep=', ')
 
-# a, b ,c = input('введите a: '), input('введите b: '), input('введите c: ')
-# print(a, b, c)
-
-# channels = [128, 200, 155]
-# r, g, b = channels
-# print(r, g, b)
-
-N = 3
-tpl = []
-for _ in range(N):
-    a, b = input('Введите фамилию: ').title(), float(input('Введите средний балл: '))
-    tpl.append((a, b))
-    # tpl.sort()
-print(tpl)
-for name in tpl:
-    a, b = name
-    print(f'Студент: {a}. Средний бал: {b}')
+# функция enumerate() - нумерует объекты
+# name = ['Сидоров', 'Петров', 'Иванов']
+# for i, v in enumerate(name, 1): # работает только в цикле
+#     print(f'{i}. {v}')
