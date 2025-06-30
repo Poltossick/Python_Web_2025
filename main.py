ec489e505c4c1a3880307fbe0327514bd8423259
Poltossick <1812.lev@gmail.com>
2025-06-30 14:57:37 +0300

    List abbreviation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from multiprocessing.pool import worker
 
 # lst = []
 # while (ingredients := input('Введите ингридиент: ')) != '': # - создаем список
@@ -10,11 +11,21 @@
 # for name in range(len(lst)):
 #     print(str(name + 1) + '.', lst[name])
 
+# lst = []
+# N = 5
+# for name in range(N):
+#     print(f'На стол положили книгу {str(name + 1)}')
+#     lst.append(name)
+# while lst:
+#     item = lst.pop() # lst.pop(0) = очередь от первой книги
+#     print(f'Со стола берем книгу {item + 1}')
+
+    # Создание аббревиатур
+    # курсы
+    # повышения
+    # квалификации
+    # КПК
 lst = []
-N = 5
-for name in range(N):
-    print(f'На стол положили книгу {str(name + 1)}')
-    lst.append(name)
-while lst:
-    item = lst.pop() # lst.pop(0) = очередь от первой книги
-    print(f'Со стола берем книгу {item + 1}')
+while (word := input('Введите слово: ').strip()) != '':
+    lst.append(word[0].upper()) # добавляем только 1 букву из введенного слова
+print(*lst, sep='') # " * " -- множество аргументов
