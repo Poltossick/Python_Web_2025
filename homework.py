effbb046aa5a06c94db0a1ae09de5f28a713b677
Polick <1812.lev@gmail.com>
2025-06-30 22:41:24 +0300

    дз 30062025

--- homework.py
+++ homework.py
@@ -1,3 +1,15 @@
-temp = 37.7
-temp2 = int(temp) + 1
-print('Температура', temp, '- это много! Это почти', str(temp2) + '.')
+stop_words = ['ну', 'типо', 'короче']
+text = ''
+lst = ''
+while (message := input('Введите сообщение: ')) != '': #ну я типо вообще короче не понимаю это язык
+    lst = message.split()
+for item in lst:
+    if item in stop_words:
+        item = ''
+    else:
+        text += item + ' '
+res = ' '.join(text.split())
+print(res)
+res = sorted(res.split())
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')
