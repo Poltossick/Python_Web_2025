1c4192fc87d301536c8fa6487e371df8008bf3e4
Poltossick <1812.lev@gmail.com>
2025-06-30 14:44:16 +0300

    List Stack

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 
-lst = []
-while (ingredients := input('Введите ингридиент: ')) != '': # - создаем список
-    lst.append(ingredients)
-temp = set(lst)  # чтобы убрать повторы
-lst = list(temp)  # чтобы убрать повторы
-print(f'Всего {len(lst)} ингредиентов: ')
+# lst = []
+# while (ingredients := input('Введите ингридиент: ')) != '': # - создаем список
+#     lst.append(ingredients)
+# temp = set(lst)  # чтобы убрать повторы
+# lst = list(temp)  # чтобы убрать повторы
+# print(f'Всего {len(lst)} ингредиентов: ')
+#
+# lst.sort()
+# for name in range(len(lst)):
+#     print(str(name + 1) + '.', lst[name])
 
-lst.sort()
-for name in range(len(lst)):
-    print(str(name + 1) + '.', lst[name])
+lst = []
+N = 5
+for name in range(N):
+    print(f'На стол положили книгу {str(name + 1)}')
+    lst.append(name)
+while lst:
+    item = lst.pop() # lst.pop(0) = очередь от первой книги
+    print(f'Со стола берем книгу {item + 1}')
