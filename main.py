a21382c0823f2a177087f4330402a71950c22b0e
Poltossick <1812.lev@gmail.com>
2025-07-01 10:24:51 +0300

    Разбор ДЗ (stop words) + commas

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-stop_words = ['ну', 'типо', 'короче']
-text = []
-message = input('Введите сообщение: ') #  ну я типо вообще короче не понимаю это язык
-lst = message.split() # все слова
-for item in lst:
-    if item not in stop_words:
-        text.append(item)
-res = sorted(text)
-for a, b in enumerate(res, 1):
-    print(f'{a}. {b}')
-
-
-stop_words = {'ну', 'типо', 'короче'}
-text = set()
-message = input('Введите сообщение: ') #  ну я типо вообще короче не понимаю это язык
-lst = message.split() # все слова
-for item in lst:
-    if item not in stop_words:
-        text.add(item)
-res = sorted(text)
-for a, b in enumerate(res, 1):
-    print(f'{a}. {b}')
+commas = (',', '.', '!', '?', '-')
 
 stop_words = {'ну', 'типо', 'короче', 'не'}
-message = input('Введите сообщение: ')  # ну я типо вообще короче не понимаю это язык
+message = input('Введите сообщение: ')  # ну я, типо! вообще - короче? не понимаю. это язык
+for z in commas:
+    message = message.replace(z, '')
 lst = message.split()  # все слова
 text = set(lst) - stop_words
 res = sorted(text)
