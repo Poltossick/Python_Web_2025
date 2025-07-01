7efeea37c3820730d0e301a39e01f612600d175f
Poltossick <1812.lev@gmail.com>
2025-07-01 10:17:04 +0300

    Разбор ДЗ (stop words)

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+stop_words = ['ну', 'типо', 'короче']
+text = []
+message = input('Введите сообщение: ') #  ну я типо вообще короче не понимаю это язык
+lst = message.split() # все слова
+for item in lst:
+    if item not in stop_words:
+        text.append(item)
+res = sorted(text)
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')
 
+
+stop_words = {'ну', 'типо', 'короче'}
+text = set()
+message = input('Введите сообщение: ') #  ну я типо вообще короче не понимаю это язык
+lst = message.split() # все слова
+for item in lst:
+    if item not in stop_words:
+        text.add(item)
+res = sorted(text)
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')
+
+stop_words = {'ну', 'типо', 'короче', 'не'}
+message = input('Введите сообщение: ')  # ну я типо вообще короче не понимаю это язык
+lst = message.split()  # все слова
+text = set(lst) - stop_words
+res = sorted(text)
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')
