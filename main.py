6de751ba457505509a289ad4cfee61ee880dcd71
Poltossick <1812.lev@gmail.com>
2025-07-01 16:12:58 +0300

    Function Return value

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 # Функции
-# позволяют реализовать принцип сухого (DRY - dont repeat yourself)
-# СИНТАКСИС :
-# def <имя функции>([параметры]):
-#     команды
-#     команды по очереди относящиеся к функции
-# 1-ый отступ по рер8
-# 2-ой отступ по рер8
-# <имя функции>('аргумент') -- вызов функции
+# Return value = завершение работы функции
 
-person = 'Max' # input('Введите имя: ') # scope(global) глобальная перемена
-count = 0 # scope(global) глобальная перемена
+def square(num):
+    return num ** 2
 
-def greet(name='user'): # scope(local) - локальная переменная    print('Привет, ', name)
-    print('Hello,', name)
-    print(count)
+def even_odd(num):
+    if num % 2 == 0:
+        return 'четное'
+    return 'нечетное'
 
-def increment():
-    global count # для воздействия на глобальную переменную
-    count += 1
+def print_string(s=None):
+    if s is None:
+        return
+    print(str)
 
-def print_list(array):
-    for item in array:
-        print(item)
-
-greet()
-greet(person)
-increment()# меняет глобальную переменную после вызова функции
-print_list(['meow', 'woof'])
+temp = square(5)
+print(temp)
+print(even_odd(temp))
