064fceb7bca0721a8fef0a0b5cbf30572b7deec9
Poltossick <1812.lev@gmail.com>
2025-07-02 16:08:09 +0300

    Filter

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-# args (arguments) and kwargs (keyword arguments пишется как имя=значение)
-# def print_any(*args, **kwargs):
-#     for item in args:
-#         print(item)
-#     for k, v in kwargs.items():
-#         print(k, '=', v)
-#
-#
-# print_any('Petr', 'Filatov', city='Moscow', age=27)
+# Функция, как объект
+# передается в другие функции -> функции высшего порядка
 
-def profile(name, surname, city, *children, **additional):
-    print(f'Имя = {name}')
-    print(f'Фамилия = {surname}')
-    print(f'Город = {city}')
-    if len(children) > 0:
-        print(f'Дети: {', '.join(children)}')
-    if 'hobby' in additional:
-        print('Хобби:', ', '.join(additional['hobby']))
+# печатник = print
+# печатник(2+3)
+
+# Функция критерия отбора элементов списка
+
+def is_longer_six(word):
+    """
+    Критерий: длина слова / строки
+    :param word: список слов
+    :return: слова от 7 букв и больше
+    """
+    # if len(word) > 6:
+    #     return True
+    # return False
+    return len(word) > 6
+
+words = ['в','списке', 'останутся', 'слова', 'длина', 'которых',
+         'больше', 'шести']
+res = list(filter(is_longer_six, words)) # отфильтровали список по функции
+print(res)
+
+for word in filter(is_longer_six, words):
+    print(word)
 
 
+def is_letter_a(word):
+    """
+    Критерий: первая буква А
+    :param word: список слов
+    :return: слова на А
+    """
+    return word[0] == 'а'
 
-profile('Михаил', 'Филатов', 'Абакан', 'Мария', 'Иван',
-        hobby=['Тенис', 'Плавание'])
+fruits = ['арбуз', 'ананас', 'банан', 'груша']
+for word in filter(is_letter_a, fruits):
+    print(word)
