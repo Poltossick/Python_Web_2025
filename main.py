e231143ed7b5d6eeb781cfd524fbebcfd8578040
Poltossick <1812.lev@gmail.com>
2025-07-02 16:17:34 +0300

    Map intro

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
 # Функция, как объект
 # передается в другие функции -> функции высшего порядка
 
-# печатник = print
-# печатник(2+3)
 
-# Функция критерия отбора элементов списка
+def square(num):
+    return str(num)
 
-def is_longer_six(word):
-    """
-    Критерий: длина слова / строки
-    :param word: список слов
-    :return: слова от 7 букв и больше
-    """
-    # if len(word) > 6:
-    #     return True
-    # return False
-    return len(word) > 6
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+squares = ''.join(list(map(square, nums)))
+print(squares)
 
-words = ['в','списке', 'останутся', 'слова', 'длина', 'которых',
-         'больше', 'шести']
-res = list(filter(is_longer_six, words)) # отфильтровали список по функции
-print(res)
-
-for word in filter(is_longer_six, words):
-    print(word)
-
-
-def is_letter_a(word):
-    """
-    Критерий: первая буква А
-    :param word: список слов
-    :return: слова на А
-    """
-    return word[0] == 'а'
-
-fruits = ['арбуз', 'ананас', 'банан', 'груша']
-for word in filter(is_letter_a, fruits):
-    print(word)
