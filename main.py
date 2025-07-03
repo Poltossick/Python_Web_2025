f7976ad9c5033eac854f2ff1a98c1cb0803f485a
Poltossick <1812.lev@gmail.com>
2025-07-03 10:12:39 +0300

    Map intro [2]

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9] # -> 123456789
+# res = ''.join(map(str, nums))
+# print(res)
+
+# Критерий вхождения подстроки
+# в частности 'ан'
+fruits = ['банан', 'ананас', 'арбуз', 'киви']
+def string_contain(str):
+    return 'ан' in str
+
+print(string_contain('банан'))
+
+res = list(filter(string_contain, fruits)) # -> ['банан', 'ананас']
+print(res)
 
