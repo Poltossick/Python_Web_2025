b62e8d22067441098a308a5b46e151f5df7ae3fd
Poltossick <1812.lev@gmail.com>
2025-07-03 11:40:47 +0300

    dict comprehensive

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
-ENGLISH_ABC = [chr(ch) for ch in range(ord('a'), ord('z') + 1)]
-# print(ENGLISH_ABC)
-RUSSIAN_ABC = [chr(ch) for ch in range(ord('а'), ord('я') + 1)] + ['ё']
-# print(RUSSIAN_ABC)
-ABC = (set(ENGLISH_ABC) ^ set(RUSSIAN_ABC) ^
-       set([x.upper() for x in ENGLISH_ABC]) ^
-       set([x.upper() for x in RUSSIAN_ABC]))
-print(ABC)
+# словарные выражения -> dict comprehensive
 
-text = 'В Индии семерых инженеров уволили после постройки моста с 90-градусным поворотом.'
-res = ''.join(filter(lambda x: x in ABC ^ {' '}, text))  # -> удаление знаков препинания
-print(res)
+numbers = range(1, 11)
+squares = {n: n ** 2 for n in numbers if n % 2 == 0}
+squares = {n: n ** 2 for n in range(1, 11) if n % 2 == 0}
+print(squares)
 
+sours_dict = {
+    'x': 1,
+    'y': 2,
+    'z': 3,
+}
 
-def remove_punctuation(txt):
-    """
-    Функция удаления знаков препинания
-    :param txt: исходный текст
-    :return: текст без знаков препинания
-    """
-    return ''.join(filter(lambda x: x in ABC ^ {' '}, text))
-
-
-def get_words(txt):
-    """
-    Функция вывода текста списком
-    :param txt: функция удаления знаков препинания
-    :return: текст списком
-    """
-    return remove_punctuation(txt).split()
-
-
-def long_words(txt, lenght=4):
-    """
-    Фильтр слов по длине 4
-    :param txt: функция вывода текста списком
-    :param lenght: длина слова
-    :return: список слов по длине 4
-    """
-    return list(filter(lambda word: len(word) >= lenght, get_words(txt)))
-
-
-print(long_words(text))
-
+dest_dict = {v ** 2 for v in sours_dict.values()}
+print(dest_dict) # -> {1, 4, 9}
+dest_dict = {k: v ** 2 for k, v in sours_dict.items()}
+print(dest_dict) # -> {'x': 1, 'y': 4, 'z': 9}
