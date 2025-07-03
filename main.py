dd7bc6f4e611ff0a42068cba16aca843a8ee70f7
Poltossick <1812.lev@gmail.com>
2025-07-03 15:17:20 +0300

    Recursion, Factorial

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-# потоковый ввод - sys.stdin - итератор (подобно range), двигается только вперед,
-# чтобы завершить, нужно нажать ctrl + D
-# (!) input() - строковый ввод
-import sys
-
-# data = sys.stdin.readlines()
-# print(data) # -> ['Привет\n', 'Как дела?\n', 'Что делаешь?\n']
-text = [d.strip('\n') for d in sys.stdin.readlines()]
-# print(' - '.join(sorted(text, key=lambda s: s[1]))) # -> Привет! Как дела? Пока
-t = list(enumerate(text))
-t.sort(key=lambda y: y[1])
-index = t[0][0]
-res = sorted(text[index].split())
-print(*res, sep='-')
-
-temp = []  # индекс строки в text и число слов в виде картежа
-for indx, srtl in enumerate(text):
-    temp.append((indx, len(srtl.split())))
-temp.sort(key=lambda y: y[1])
-index = temp[0][0]
-res = sorted(text[index].split())
-print(*res, sep='-')
+# Рекурсия
+# def factorial(count): # 5! (факториал 5) = 1 * 2 * 3 * 4 * 5
+#     result = 1
+#     for i in range(2, count + 1):
+#         result *= i
+#     return result
+#
+# for x in range(6):
+#     print(x, factorial(x))
+from idlelib.tooltip import OnHoverTooltipBase
 
 
-# неверное  for line in text:
-#     if len(line) == 2:
-#         print(' - '.join(sorted(line, key=lambda s: s[0])))
-#     else:
-#         pass
+def factorial(count):
+    if count <= 1:  # -> базовый вариант
+        return 1  # -> базовый вариант
+    return count * factorial(count - 1)  # -> рекурсивная / "пружина"
+
+
+for x in range(6):
+    print(x, factorial(x))
