5acabd9874a5d39ca6daa20f48a50ad54ad7c91c
Poltossick <1812.lev@gmail.com>
2025-07-03 14:42:30 +0300

    sys.stdin потоковый ввод

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# проверка коллекций: any() & all()
-# any() - любой элемент коллекции возвращает True / False
-# all() - все элементы коллекции возвращает True / False
+# потоковый ввод - sys.stdin - итератор (подобно range), двигается только вперед,
+# чтобы завершить, нужно нажать ctrl + D
+# (!) input() - строковый ввод
+import sys
 
-# print(all([1, 2, 3, -1])) # все элементы не нулевые true
-# print(all([1, 2, 0])) # один элемент нулевой false
-# print(all([])) # все элементы не нулевые true
+# data = sys.stdin.readlines()
+# print(data) # -> ['Привет\n', 'Как дела?\n', 'Что делаешь?\n']
+text = [d.strip('\n') for d in sys.stdin.readlines()]
+# print(' - '.join(sorted(text, key=lambda s: s[1]))) # -> Привет! Как дела? Пока
+t = list(enumerate(text))
+t.sort(key=lambda y: y[1])
+index = t[0][0]
+res = sorted(text[index].split())
+print(*res, sep='-')
 
-words = 'один, два, три'.split()
-# list_for_analyze = list(map(lambda x: len(x) > 3, words))
-print(all(list(map(lambda x: len(x) > 3, words)))) # -> False
-print(any(list(map(lambda x: len(x) > 3, words)))) # -> True
+temp = []  # индекс строки в text и число слов в виде картежа
+for indx, srtl in enumerate(text):
+    temp.append((indx, len(srtl.split())))
+temp.sort(key=lambda y: y[1])
+index = temp[0][0]
+res = sorted(text[index].split())
+print(*res, sep='-')
+
+
+# неверное  for line in text:
+#     if len(line) == 2:
+#         print(' - '.join(sorted(line, key=lambda s: s[0])))
+#     else:
+#         pass
