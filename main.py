0be4e46ef01b470ac4f7762c941c2162359bca25
Poltossick <1812.lev@gmail.com>
2025-07-04 12:48:55 +0300

    Модуль datetime

--- main.py
+++ main.py
@@ -1,58 +1,28 @@
 # Встроенные библиотеки
 # PyPI - Python Package Index (pypi.org)
-import random as rnd  # -> подключение библиотеки, импортировать всю библиотеку
+import datetime as dtm
+# print(dtm.datetime.now()) # сырое имя
+# print(dtm.datetime.now().date()) # дата сегодня
+# print(dtm.datetime.now().time()) # время сейчас
+# print(type(dtm.datetime.now())) # класс
 
-# print(dir(rnd)) # -> список функций для math
-"""['BPF', 'LOG4', 'NV_MAGICCONST', 'RECIP_BPF', 'Random', 'SG_MAGICCONST', 'SystemRandom', 
- 'TWOPI', '_ONE', '_Sequence', '__all__', '__builtins__', '__cached__', '__doc__', '__file__', 
- '__loader__', '__name__', '__package__', '__spec__', '_accumulate', '_acos', '_bisect', '_ceil', '_cos', 
- '_e', '_exp', '_fabs', '_floor', '_index', '_inst', '_isfinite', '_lgamma', '_log', '_log2', '_os', '_pi', 
- '_random', '_repeat', '_sha512', '_sin', '_sqrt', '_test', '_test_generator', '_urandom', '_warn', 'betavariate', 
- 'binomialvariate', 'choice', 'choices', 'expovariate', 'gammavariate', 'gauss', 'getrandbits', 'getstate', 
- 'lognormvariate', 'normalvariate', 'paretovariate', 'randbytes', 'randint', 'random', 'randrange', 'sample', 
- 'seed', 'setstate', 'shuffle', 'triangular', 'uniform', 'vonmisesvariate', 'weibullvariate'] 
-"""
-# lst = ['орёл', 'решка']
-# print(rnd.choice(lst))
-# print(rnd.randint(0, 10))
-# print(rnd.randrange(0, 10, 3))
+# strftime() - string format time
+
+# time = dtm.datetime.now()
+# ftime = time.strftime('%d/%m/%Y-%B') # -> 04/07/2025-July - день, год, месяц m - месяц числом, B - месяц словом на англ
+# print(ftime)
+# ftime = time.strftime('%H:%M') # -> 12:32
+# print(ftime)
 #
-# dct = {
-#     'a': 'cat',
-#     'b': 'dog',
-#     'c': 'rabbit',
-#     'd': 'rat',
-# }
-# keys = list(dct.keys())
-# key = rnd.choice(keys)
-# print(dct[key])
-# ENGLISH_ABC = [chr(ch) for ch in range(ord('a'), ord('z') + 1)]
-# zara = ['\u2680', '\u2681', '\u2682','\u2683','\u2684','\u2685'] # -> ⚃ ⚁
-# for i in range(2):
-#     print('choice ', rnd.choice(zara), rnd.choice(zara))
-#
-# for i in range(2):
-#     print('sample', rnd.sample(ENGLISH_ABC, k=3))
-# rnd.shuffle(ENGLISH_ABC)
-# print(ENGLISH_ABC)
+# my_time = dtm.time(15, 27, 32)
+# my_day = dtm.date(2025, 12, 5).strftime('%d %B %Y')
+# my_d_t = dtm.datetime.combine(dtm.date(2025, 12, 5), my_time)
+# print(my_time)
+# print(str(my_day))
+# print(my_d_t)
 
-# ENGLISH_ABC = [chr(ch) for ch in range(ord('a'), ord('z') + 1)]
-# lst = ENGLISH_ABC + [range(0, 10)] + ['!', '@', '/', '%']
-# rnd.shuffle(lst)
-# password = ''.join(str(lst[:8]))
-# print(password)
+date_1 = dtm.date(2025, 6, 15)
+date_2 = dtm.date(2025, 7, 3)
+print(date_2 - date_1)
 
 
-ENGLISH_ABC = list(chr(ch) for ch in range(ord('a'), ord('z') + 1))
-abc = list(set(ENGLISH_ABC) ^ set([x.upper() for x in ENGLISH_ABC]))
-simbols = list(['!', '@', '/', '%'])
-numbers = '1', '2', '3', '4', '5', '6', '7', '8', '9'
-N = 8
-temp = abc[:N - 3]
-temp.append(rnd.choice(simbols))
-temp.append(rnd.choice(numbers))
-rnd.shuffle(temp)
-password = ''.join(temp)
-print(password)
-
-rnd.seed()
