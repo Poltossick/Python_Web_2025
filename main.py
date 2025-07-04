316952f7ef4233ff997db55bb427a6bcd56bf115
Poltossick <1812.lev@gmail.com>
2025-07-04 10:31:55 +0300

    sum, min, max // average = нет в питоне

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+# result = sum(1, 2, 3) - ошибка, только итерируемый объект
+lst = [1, 2, 3]
+res = 0
+for x in lst:
+    res += x
+print(res)
+result = sum(lst)
+min_v = min(lst)
+max_v = max(lst)
+print(result, min_v, max_v)
