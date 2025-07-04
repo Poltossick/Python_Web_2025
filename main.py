3ecfb60cc0590a86c53ee53b99b068dd14d9f437
Poltossick <1812.lev@gmail.com>
2025-07-04 16:17:41 +0300

    Image Draw

--- main.py
+++ main.py
@@ -1,43 +1,17 @@
 # Работа с изображением - растровое изображение
 # thumbnail - скрипт на уменьшение изображения для публикации на сайте микро-изображения
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-img = Image.open('images/python.jpg')
+image = Image.new('RGB', (600, 400), (0, 0, 255))  # -> создание холста
 
-# print(img.size) # -> свойство возвращает картеж (800, 600)
-x, y = img.size  # -> x (w) -- ширина (с лева на право), y (h) -- высота (сверху вниз)
-# print('Ширина (х) - ', x, 'Высота (y)', y) # -> Ширина (х) -  800 Высота (y) 600
-mode = img.mode
-# print('Цветовая схема (mode) - ', mode) # -> свойство - Цветовая схема (mode) -  RGB
+POLY = [(50, 50), (150, 50), (50, 150)]
 
+draw = ImageDraw.Draw(image)
+draw.rectangle((10, 10, 590, 390), fill=(0, 192, 192), outline=(255, 255, 255), width=10)
+draw.ellipse((15, 15, 585, 385), fill=(180, 170, 180), outline=(0, 0, 0), width=10)
+draw.line((0, 0, 600, 400), fill=(15, 150, 0), width=5)
+draw.line((0, 400, 600, 0), fill=(150, 15, 0), width=5)
+draw.polygon(POLY, outline='white', width=12)
+draw.text((100, 100), 'Hello, word', fill=(0, 0, 0), font_size=66)
 
-pixels = img.load()  # -> загрузить таблицу пикселей
-
-# инверсия
-# for i in range(x):
-#     for j in range(y):
-#         r, g, b = pixels[i, j] # -> обращение в виде картежа
-#         pixels[i, j] = g, b, r
-
-# негатив
-# for i in range(x):
-#     for j in range(y):
-#         r, g, b = pixels[i, j]  # -> обращение в виде картежа
-#         pixels[i, j] = 255 - r, 255 - g, 255 - b
-
-# Grayscale - оттенки серого - черно-белая картинка
-# (0,0,0) - (1,1,1) - (245,245,245)
-# for i in range(x):
-#     for j in range(y):
-#         r, g, b = pixels[i, j]  # -> обращение в виде картежа
-#         average = (r + g + b) // 3
-#         pixels[i, j] = average, average, average
-img.save('images/python2.jpg')
-
-
-img.transpose(Image.Transpose.FLIP_LEFT_RIGHT).save('images/python_FLIP_LEFT_RIGHT.jpg')
-img.transpose(Image.Transpose.FLIP_TOP_BOTTOM).save('images/python_TOP_BOTTOM.jpg')
-img.rotate(30).save('images/python_rotated.jpg')
-img.crop((250, 0, 550, 200)).save('images/python_cropped.jpg')
-img.resize((400, 300)).save('images/python_resized.jpg')
-
+image.save('./images/blue.jpg')
