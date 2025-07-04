5091dffad0bc74fcac8fa8b6ca9ae731ffe7b9d9
Poltossick <1812.lev@gmail.com>
2025-07-04 11:16:20 +0300

    Модуль math

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# result = sum(1, 2, 3) - ошибка, только итерируемый объект
-lst = [1, 2, 3]
-res = 0
-for x in lst:
-    res += x
-print(res)
-result = sum(lst)
-min_v = min(lst)
-max_v = max(lst)
-print(result, min_v, max_v)
+# Встроенные библиотеки
+# PyPI - Python Package Index (pypi.org)
+# import math as m # -> подключение библиотеки, импортировать всю библиотеку, ДИНАМИЧЕСКИЙ
+# print(dir(m)) # -> список функций для math
+# from math import *  # -> подключение библиотеки, импортировать всю библиотеку, ВСЕ ВЫТЯГИВАЕМ СРАЗУ
+from math import pi, cos, hypot  # -> подключение конкретных функция
+from math import sin, radians  # -> подключение конкретных функция
+
+print('Число Пи: ', pi)
+print(f'Косинус 5°: ', round(cos(radians(5)), 3))
+print('Синус 30°: ', round(sin(radians(30)), 2))
+print('Гипотенуза: ', hypot(3, 2))
