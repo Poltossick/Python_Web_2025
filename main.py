49ef725f1329dca2398b163d9c0b8d3ec9a7b0f8
Poltossick <1812.lev@gmail.com>
2025-07-07 11:31:20 +0300

    Docx Template

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 # Documents
 # Word - DOCX (python-docx)
+# pip install docxtpl
 from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Cm, Inches, Mm, Pt # для размеров
+from docxtpl import DocxTemplate
+import pkg_resources
 
-doc = Document()  # создание экземпляра документа
+document = DocxTemplate('documents/template.docx')
 
-# Добавление заголовка
+content = {
+    'company': 'ООО "Зеленоглазое такси"',
+    'employee': 'Петров П.П.',
+    'position': 'Директор',
+    'date' : '01/09/2024'
+}
 
-doc.add_heading('Отчет за месяц', 1)  # заголовок уровня один
-paragraph = doc.add_paragraph() # отступ
-paragraph = doc.add_paragraph('В этом отчете представлены ')
-# run - что-то внутри абзаца (текст, картинка)
-paragraph.add_run(' ключевые показатели').bold = True
-
-paragraph_format = paragraph.paragraph_format
-paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-# маркированный список
-paragraph = doc.add_paragraph('Первый пункт', style='List Bullet')
-paragraph = doc.add_paragraph('Второй пункт', style='List Bullet')
-# нумерованный список
-paragraph = doc.add_paragraph('Первый пункт', style='List Number')
-paragraph = doc.add_paragraph('Второй пункт', style='List Number')
-
-paragraph = doc.add_paragraph() # отступ
-# добавляем и заполняем таблицу
-table = doc.add_table(3, 2)
-for i, a  in enumerate(table.rows):
-    for j, b  in enumerate(table.columns):
-        b.text = f'Строка {i + 1}, Столбец {j + 1}'
-
-doc.add_paragraph() # отступ
-doc.add_picture('images/sunny_day.jpg', width=Mm(50))
-
-
-
-doc.save('./documents/report.docx')
+document.render(content)
+document.save('./documents/about.docx')
