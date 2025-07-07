e8a71c9e873aef3904de1fd212b0708c2fe5f494
Poltossick <1812.lev@gmail.com>
2025-07-07 09:54:26 +0300

    Sunny day

--- main.py
+++ main.py
@@ -2,16 +2,12 @@
 # thumbnail - скрипт на уменьшение изображения для публикации на сайте микро-изображения
 from PIL import Image, ImageDraw, ImageFont
 
-image = Image.new('RGB', (600, 400), (0, 0, 255))  # -> создание холста
+image = Image.new('RGB', (600, 400), (0, 191, 255))  # -> создание холста
 
 POLY = [(50, 50), (150, 50), (50, 150)]
 
 draw = ImageDraw.Draw(image)
-draw.rectangle((10, 10, 590, 390), fill=(0, 192, 192), outline=(255, 255, 255), width=10)
-draw.ellipse((15, 15, 585, 385), fill=(180, 170, 180), outline=(0, 0, 0), width=10)
-draw.line((0, 0, 600, 400), fill=(15, 150, 0), width=5)
-draw.line((0, 400, 600, 0), fill=(150, 15, 0), width=5)
-draw.polygon(POLY, outline='white', width=12)
-draw.text((100, 100), 'Hello, word', fill=(0, 0, 0), font_size=66)
+draw.ellipse((500, -100, 700, 100), fill=(255, 255, 0), outline=(255, 255, 0), width=50)
+draw.text((150, 150), 'Sunny day', fill=(255, 255, 0), font_size=66)
 
-image.save('./images/blue.jpg')
+image.save('./images/DAY.jpg')
