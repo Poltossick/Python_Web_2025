3782df548fa39fa1fc31cbcb2ce4785e58fdbc09
Poltossick <1812.lev@gmail.com>
2025-07-07 16:13:40 +0300

    OS: File list

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 # ОС-модуль - работа с операционной системой
 
 import os
-
-# os.mkdir('libs') # -> создание директории
-# os.makedirs('libs', exist_ok=True) # -> 'мягкое' создание директории, вместо  os.mkdir('libs')
-# os.rmdir('libs')  # -> удаление директории
-# print(os.path.exists('libs'))  # -> проверить наличие директории
-# if os.path.exists('libs'):
-#     os.rmdir('libs')  # -> 'мягкое' удаление директории
-
-path = os.getcwd()  # -> get current working directory
-print(os.getcwd())
-
+path = os.getcwd()
 os.chdir(path + '/images')
-print(os.getcwd())
 
-os.chdir('..')
-os.chdir(path + '/fonts')
-print(os.getcwd())
+all_files = [f for f in os.listdir('.') if f.startswith('py')]
+# обращаемся к конкретной директории ('.') - выше ее поменяли
+print(all_files)
