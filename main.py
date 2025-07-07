a1568bb26e3eb1360a34091d24d0c2aaeb921bb9
Poltossick <1812.lev@gmail.com>
2025-07-07 16:08:16 +0300

    OS: Intro

--- main.py
+++ main.py
@@ -1,62 +1,20 @@
-# Файлы
-# t - текстовые файлы (txt, html, xml)
-# b - бинарный файлы (jpg, avi, mp3)
-# w - write (запись файла, создается) - открывается очищенный файл
-# a - append (если не существует - создается, если существует - открывается весь с последними изменениями)
-# r - read (файл должен существовать, открывается ТОЛЬКО на прочтение) - по умолчанию
+# ОС-модуль - работа с операционной системой
 
-# file_object = open('info.txt', 'wt', encoding='utf-8') # открывает файл
-# # print(file_object.mode)
-# # print(file_object.name)
-# # print(file_object.encoding) -> encoding='utf-8'
-#
-# count = file_object.write('Этот текст будет в файле')
-# print(f'В файл записан {count} байт!')
-#
-# file_object.close()
+import os
 
-# file_obj = open('info.txt', 'rt', encoding='utf-8')
-#
-# text = file_obj.read(3)# -> в скобках пишется сколько байт читать
-# file_obj.read(6+7)
-# text += file_obj.read(8)
-# print('Вот что было в файле' , end=': ')
-# print(text)
-#
-# file_obj.close()
+# os.mkdir('libs') # -> создание директории
+# os.makedirs('libs', exist_ok=True) # -> 'мягкое' создание директории, вместо  os.mkdir('libs')
+# os.rmdir('libs')  # -> удаление директории
+# print(os.path.exists('libs'))  # -> проверить наличие директории
+# if os.path.exists('libs'):
+#     os.rmdir('libs')  # -> 'мягкое' удаление директории
 
-# file_obj = open('info.txt', 'at', encoding='utf-8')
-# file_obj.write(f'\nЭто хороший текст!')
-# file_obj.close()
+path = os.getcwd()  # -> get current working directory
+print(os.getcwd())
 
-# file_obj = open('info.txt', 'at', encoding='utf-8')
-#
-# print('\nА вот еще одна строка.', file=file_obj)
-#
-# file_obj.close()
+os.chdir(path + '/images')
+print(os.getcwd())
 
-# file_obj = open('info.txt', 'rt', encoding='utf-8')
-#
-# while text := file_obj.readline():
-#     for line, str in enumerate(file_obj, 1):
-#         print(f'{line}. {str.rstrip('\n')}')
-#
-#
-# # lst = file_obj.readlines()
-# # lst = list(map(lambda x: x.strip('\n'), lst))
-# # print(lst)
-#
-# # text = file_obj.read()
-# # lst = text.splitlines()
-# # print(lst)
-#
-# file_obj.close()
-
-
-
-# открытие с менеджером контекста, проследит, чтобы файл закрылся
-with open('info.txt', 'rt', encoding='utf-8') as file_obj:
-    while text := file_obj.readline():
-        for line, str in enumerate(file_obj, 1):
-            print(f'{line}. {str.rstrip('\n')}')
-
+os.chdir('..')
+os.chdir(path + '/fonts')
+print(os.getcwd())
