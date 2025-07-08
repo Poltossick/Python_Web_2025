e8cc92ebee3dde94d2bfbd930cb87e2b653d9c26
Poltossick <1812.lev@gmail.com>
2025-07-08 12:11:18 +0300

    Exception - Raise

--- main.py
+++ main.py
@@ -13,18 +13,34 @@
 #     file_object.close()
 # finally:
 #     print('Продолжаем работать.')
-print('Остаток от деления: ')
+
+# print('Остаток от деления: ')
+# loop = True # цикл
+# while loop:
+#     try:
+#         value = int(input('На что делим число 10: '))
+#         res = 10 % value
+#         print(f'остаток от деления 10 на {value} = {res}')
+#     except ZeroDivisionError:
+#         print('На ноль делить нельзя!')
+#     # except ValueError:
+#     #     print('Надо вводить только целые числа')
+#     except Exception as exp:
+#         print('Произошло исключение: ', exp.__class__.__name__, ' - ', exp)
+#     else:
+#         loop = False
+
+#  'Бросаемся' исключениями - raise (throw в других языках)
+max_val = 10
+min_val = 1
 loop = True # цикл
 while loop:
     try:
-        value = int(input('На что делим число 10: '))
-        res = 10 % value
-        print(f'остаток от деления 10 на {value} = {res}')
-    except ZeroDivisionError:
-        print('На ноль делить нельзя!')
-    # except ValueError:
-    #     print('Надо вводить только целые числа')
-    except Exception as exp:
-        print('Произошло исключение: ', exp.__class__.__name__, ' - ', exp)
+        val = int(input(f'Введите целое число от {min_val} до {max_val}: '))
+        if not min_val < val < max_val:
+            raise ValueError('введенное число вне диапазона')
+        print(f'Введенное число {val} лежит в заданном диапазоне')
+    except ValueError as exp:
+        print('Надо быть внимательнее:', exp)
     else:
         loop = False
