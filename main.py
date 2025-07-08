d04e43c9cc8dff0f2537ba789043d771fc8a11fb
Poltossick <1812.lev@gmail.com>
2025-07-08 12:58:40 +0300

    Exception - assertion + задачи

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-# Исключения - (runtime - время исполнения программы) except
-# print(name) # -> NameError: name 'name' is not defined
-# file_object = open('information.txt') # -> FileNotFoundError: [Errno 2] No such file or directory: 'information.txt'
-# try:
-#     file_object = open('information.txt', encoding='utf-8')
-# except FileNotFoundError:
-#     print('Файл не найден. Файл создан с параметрами по умолчанию')
-#     with open('information.txt', 'wt', encoding='utf-8') as file_object:
-#         file_object.write('По умолчанию')
-# else:
-#     print('Файл открыт успешно. Читаем его и закрываем.')
-#     print('Текст в файле: ', file_object.read())
-#     file_object.close()
-# finally:
-#     print('Продолжаем работать.')
+# Задача 1
 
-# print('Остаток от деления: ')
-# loop = True # цикл
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# loop = True
 # while loop:
 #     try:
-#         value = int(input('На что делим число 10: '))
-#         res = 10 % value
-#         print(f'остаток от деления 10 на {value} = {res}')
-#     except ZeroDivisionError:
-#         print('На ноль делить нельзя!')
-#     # except ValueError:
-#     #     print('Надо вводить только целые числа')
-#     except Exception as exp:
-#         print('Произошло исключение: ', exp.__class__.__name__, ' - ', exp)
+#         index = int(input('Введите индекс: '))
+#         print(f'Число по индексу {index}: {lst[index]}')
+#     except ValueError:
+#         print('Индекс вне диапазона')
+#     except IndexError:
+#         print('Индекс от 0 до 8 включительно')
+#     # except Exception as exp:
+#     #     print('Ошибка', exp.__class__.__name__, ':', exp)
 #     else:
 #         loop = False
 
-#  'Бросаемся' исключениями - raise (throw в других языках)
-max_val = 10
-min_val = 1
-loop = True # цикл
-while loop:
+# Задача 2
+# while True:
+#     a = input('Введите первое число: ')
+#     b = input('Введите второе число: ')
+
+# while True:
+#     a = input('Введите первое число: ')
+#     b = input('Введите второе число: ')
+#     try:
+#         if not int(b) != 0:
+#             raise ZeroDivisionError ('На ноль делить нельзя')
+#         else:
+#             print('Ответ:', int(a) / int(b))
+#             break
+#     except ZeroDivisionError as expt:
+#         print(expt)
+#     except ValueError:
+#         print('Должно быть число')
+
+while True:
+    a = input('Введите первое число: ')
+    b = input('Введите второе число: ')
     try:
-        val = int(input(f'Введите целое число от {min_val} до {max_val}: '))
-        if not min_val < val < max_val:
-            raise ValueError('введенное число вне диапазона')
-        print(f'Введенное число {val} лежит в заданном диапазоне')
-    except ValueError as exp:
-        print('Надо быть внимательнее:', exp)
+        res = int(a) / int(b)
+    except ZeroDivisionError:
+        print('На ноль делить нельзя')
+    except ValueError:
+        print('Должно быть число')
     else:
-        loop = False
+        print('Ответ:', res)
+        break
+
+
+
