e450d266d2339a29e4a50cc34f0ca3c445f91036
Poltossick <1812.lev@gmail.com>
2025-07-08 15:00:41 +0300

    PyMorphy

--- main.py
+++ main.py
@@ -1,56 +1,26 @@
-# Практикум (обучаемый словарь)
-import pickle
+# Библиотека pymorphy
+import pymorphy3
 
-# минимальная выерсия, если файл dict.dat отсутствует
-vocabulary = {
-    'стол': 'table',
-    'стул': 'chair',
-}
+# morph = pymorphy3.MorphAnalyzer() # -> морфологический анализ слова
+#
+# print(morph.parse('Дмитрий'))
+# """
+# [Parse(word='дмитрий', tag=OpencorporaTag('NOUN,anim,masc,Name sing,nomn'),
+# normal_form='дмитрий', score=0.985915, methods_stack=((DictionaryAnalyzer(),
+# 'дмитрий', 61, 0),)), Parse(word='дмитрий', tag=OpencorporaTag('NOUN,anim,femn,Name plur,gent'),
+# normal_form='дмитрия', score=0.007042, methods_stack=((DictionaryAnalyzer(), 'дмитрий', 64, 8),)),
+# Parse(word='дмитрий', tag=OpencorporaTag('NOUN,anim,femn,Name plur,accs'), normal_form='дмитрия',
+# score=0.007042, methods_stack=((DictionaryAnalyzer(), 'дмитрий', 64, 10),))]
+# """
 
-def print_voc():
-    """
-    Функция для распечатки словаря
-    :return: выводит словарь
-    """
-    print('Сейчас словарь содержит: ')
-    for k, v in vocabulary.items():
-        print(k, '—', v) # -> — Alt + 0151
+from pymorphy3 import MorphAnalyzer
 
-
-
-try:
-    with open('dict.dat', 'rb') as dump_in:
-        vocabulary = pickle.load(dump_in)
-        print_voc()
-except FileNotFoundError:
-    with open('dict.dat', 'wb') as dump_out:
-        pickle.dump(vocabulary, dump_out)
-    print('Создан минимальный словарь: ')
-    print_voc()
-
-
-while True:
-    word = input('\nВведите слово для перевода или # для завершения: ').strip().lower()
-    if word == '#' or word == '№':
-        break
-    if word in vocabulary.keys():
-        translate = vocabulary[word]
-        print(f'Слово "{word}" переводится как "{translate}".\n')
+form = MorphAnalyzer().parse('бутылка')[0]
+for btl in reversed(range(99)):
+    print(f'В холодильнике {btl + 1} {form.make_agree_with_number(btl + 1).word} пива.')
+    print('Возьмем одну и выпьем')
+    if btl % 10 == 1 and btl != 11:
+        remain = 'Осталась '
     else:
-        print(f'Значение слова "{word}" отсутствует в словаре.\n')
-        new_key = f'А как слово "{word}" переводится.  \n'
-        new_key += f'Если ничего не вводите нажмите ENTER, \n'
-        new_key += f'или введите его здесь: '
-        new_word = input(new_key)
-
-        if new_word != '' or len(new_word) > 1:
-            vocabulary[word] = new_word
-            print(f'Слово "{word}" с переводом "{new_word}" внесено в словарь.')
-        else:
-            print('Ничего не введено или слишком короткое слово')
-            continue
-
-with open('dict.dat', 'wb') as dump_out:
-    pickle.dump(vocabulary, dump_out)
-
-print('До новых встреч!')
+        remain = 'Осталось '
+    print(f'{remain}{btl} {form.make_agree_with_number(btl).word} пива.')
