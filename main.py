2ce04c118d6aa51b8a1a0942b970f89a2cdd6651
Poltossick <1812.lev@gmail.com>
2025-07-08 11:30:18 +0300

    Exception Intro

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-# Сериализация
+# Исключения - (runtime - время исполнения программы) except
+# print(name) # -> NameError: name 'name' is not defined
+# file_object = open('information.txt') # -> FileNotFoundError: [Errno 2] No such file or directory: 'information.txt'
+# try:
+#     file_object = open('information.txt', encoding='utf-8')
+# except FileNotFoundError:
+#     print('Файл не найден. Файл создан с параметрами по умолчанию')
+#     with open('information.txt', 'wt', encoding='utf-8') as file_object:
+#         file_object.write('По умолчанию')
+# else:
+#     print('Файл открыт успешно. Читаем его и закрываем.')
+#     print('Текст в файле: ', file_object.read())
+#     file_object.close()
+# finally:
+#     print('Продолжаем работать.')
 
-import pickle
-import pprint
-
-# dic = {
-#     'table': 'стол',
-#     'chair': 'стул'
-# }
-#
-# with open('./documents/dictfile.dat', 'wb') as pcl:
-#     # dic - что сериализуем
-#     # pcl - куда сериализуем
-#     pickle.dump(dic, pcl)
-
-# Десериализация
-
-# with open('./documents/dictfile.dat', 'rb') as pcl:
-#     # dic - что будет загружено
-#     # pcl - откуда будет загружено
-#     dic = pickle.load(pcl)
-#
-# pprint.pprint(dic, width=20)
-
-from path_lib import *
-print(img_dir)
-print(font_dir)
-print(doc_dir)
-print(test)
+flag = False
+try:
+    file_object = open('information.txt', encoding='utf-8')
+except FileNotFoundError:
+    file_object = open('information.txt', 'wt', encoding='utf-8')
+    flag = True
+    print('Файл не найден. Файл создан с параметрами по умолчанию')
+else:
+    print('Файл открыт успешно. Читаем его и закрываем.')
+    print('Текст в файле: ', file_object.read())
+    file_object.close()
+finally:
+    if flag:
+        file_object.write('По умолчанию')
+        file_object.close()
+    print('Продолжаем работать.')
