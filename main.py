b55e1c0f1cd2fd6e87bef9dcdc070edfe477a7f6
Poltossick <1812.lev@gmail.com>
2025-07-09 11:19:10 +0300

    ReExp Punctuation (sub, split, match)

--- main.py
+++ main.py
@@ -3,32 +3,29 @@
 # r-строка — raw-string (сырая строка)
 import re
 
-# pattern = r'стеклянн?ый' # вторая "н" может присутствовать, но не обязана
-# test_string = 'стекляный, стеклянный, оловянный, деревянный, серебряный'
-# result = re.findall(pattern, test_string)
-# print(result) # -> ['стекляный', 'стеклянный']
-
-# pattern = r'<img.*' # "жадный квантификатор" (greedy quantifier)
-# test_string = 'Картинка <img src="bg.jpg"> в тексте</p>' # ['<img src="bg.jpg"> в тексте</p>']
-# result = re.findall(pattern, test_string)
-# print(result)
+# def remove_punctuation(input_str: str) -> str:
+#     """
+#     Методом sub() заменяем все найденный совпадения пустой строкой
+#     и возвращаем очищенную
+#     :param input_str: строка со знаками препинания
+#     :return: строку, очищенную от этих знаков препинания
+#     """
+#     return re.sub(r'[^\w\s]', '', input_str)
 #
-# pattern = r'<img.*?>' # "ленивый квантификатор" (non-greedy, lazy quantifier)
-# test_string = 'Картинка <img src="bg.jpg"> в тексте</p>'  # ['<img src="bg.jpg">']
-# result = re.findall(pattern, test_string)
+#
+# test_string = 'Язык Python, явл?яясь инту"итивно понятным, пр,ост для изучения! Ну и PEP8/'
+# result = remove_punctuation(test_string)
 # print(result)
 
-# pattern = r'<img[^>]+src="([^">]+)"' # только путь к картинке
-# test_string = 'Картинка <img src="bg.jpg"> в тексте</p>'  # ['bg.jpg']
-# result = re.findall(pattern, test_string)
+# pattern = r'[^\w\s]'
+# test_string = 'яблоко,груша;банан!абрикос?слива'
+# result = re.split(pattern, test_string)
 # print(result)
 
-# pattern = r'<p>(.*?)</p>' # без "?" выпадет <i>и т.д.</i>
-# test_string = '<b>Вот начало: </b><p>Содержимое</p><i>и т.д.</i><p>документа</p>'  # ['Содержимое', 'документа']
-# result = re.findall(pattern, test_string)
-# print(result)
+# pattern = r'[,:;!?.]'
+# test_string = 'яблоко,   груша;   банан!   абрикос?   слива  '
+# result = re.split(pattern, test_string)
+# print(''.join(result).split())  # -> ['яблоко', 'груша', 'банан', 'абрикос', 'слива']
+# print((list(map(lambda x: x.strip(), result))))  # через функцию map и lambda
+# print([x.strip() for x in result])  # списочное выражение
 
-pattern = r'<p[^>]*>(.*?)</p>' # содержимое сайта с атрибутами align="center"
-test_string = '<b>Центрируем</b><p align="center">Содержимое</p>'
-result = re.findall(pattern, test_string)
-print(result)
