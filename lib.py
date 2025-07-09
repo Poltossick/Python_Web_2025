937e4e5383f047abfda28bbb7f94f4b521c4c605
Poltossick <1812.lev@gmail.com>
2025-07-09 15:46:50 +0300

    OOP Getter & Setter Task 1 — Clicker

--- lib.py
+++ lib.py
@@ -1,12 +1,52 @@
+class Clicker:
+    def __init__(self):
+        self._counter = 0
+
+    def click(self):
+        self._counter +=1
+
+    def click_counter(self):
+        return self._counter
+
+    def reset(self):
+        self._counter = 0
+
+
+
+
 class Car:
+    count = 0
+
     def __init__(self, brand='N/A', model='N/A', color='N/A'):
         self.brand = brand
         self.model = model
         self.color = color
         self.engine_on = False
+        Car.count += 1
 
     def start_engine(self):
-        self.engine_on = True # пока не сработает
+        self.engine_on = True  # пока не сработает
+
+    def set_brand(self, new_brand):  # setter
+        if new_brand:
+            self.brand = new_brand
+
+    def set_model(self, new_model):  # setter
+        if new_model:
+            self.model = new_model
+
+    def set_color(self, new_color):  # setter
+        if new_color:
+            self.color = new_color
+
+    def get_brand(self):  # getter
+        return f'Бренд авто — {self.brand}'
+
+    def get_model(self):  # getter
+        return f'Модель авто — {self.model}'
+
+    def get_color(self):  # getter
+        return f'Цвет авто — {self.color}'
 
     def drive_to(self, place):
         if self.engine_on:
@@ -14,13 +54,39 @@ class Car:
         else:
             print('Заведите автомобиль')
 
+    @staticmethod
+    def get_count():
+        return Car.count
 
 
+# __________________________________________________________________________________
+class Person:
+    def __init__(self, name='N/A', age='N/A'):
+        self._name = name
+        self._age = age
 
+    def set_name(self, new_name):  # setter
+        if new_name:
+            self._name = new_name
 
+    def set_age(self, new_age):  # setter
+        if 0 < new_age < 150:
+            self._age = new_age
+        else:
+            print('Некорректный возраст — ', new_age)
 
-def summ(a, b):
-    return a + b
+    def get_name(self):  # getter
+        return f'Имя — {self._name}'
+
+    def get_age(self):  # getter
+        return f'Возраст — {self._age}'
+
+    def person_info(self):
+        print(f'Данные пользователя:\nИмя - {self._name}, возраст {self._age}')
+
+    # __________________________________________________________________________________
+    def summ(a, b):
+        return a + b
 
 
 def diff(a, b):
--- main.py
+++ main.py
@@ -2,27 +2,15 @@
 # инкапсуляция - сокрытие внутренней реализации с целью сокрытия внутренних данных
 
 # АНАЛИЗ предыдущих вызовов
-from lib import Car
+from lib import Car, Person, Clicker
 
-car = Car('BMW', 'X5')
-car.start_engine()
-car.drive_to('город')
+a = Clicker()
+a.click()
+a.click()
+a.click()
+a.click()
+a.click()
+print(a.click_counter())
 
-"""
-УНЕСТИ В LIB.PY
-class Car:
-    def __init__(self, brand='N/A', model='N/A', color='N/A'):
-        self.brand = brand
-        self.model = model
-        self.color = color
-        self.engine_on = False
-
-    def start_engine(self):
-        self.engine_on = True # пока не сработает
-
-    def drive_to(self, place):
-        if self.engine_on:
-            print(f'Едем в {place} на {self.brand} {self.model}')
-        else:
-            print('Заведите автомобиль')
-"""
+a.reset()
+print(a.click_counter())
