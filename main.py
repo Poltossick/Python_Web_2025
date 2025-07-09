0922d7175ad76d6a75c78c991b84542cf6c58014
Poltossick <1812.lev@gmail.com>
2025-07-09 11:29:41 +0300

    import requests

--- main.py
+++ main.py
@@ -2,30 +2,11 @@
 # Regular Expression - папка при написании "re"
 # r-строка — raw-string (сырая строка)
 import re
+import requests
 
-# def remove_punctuation(input_str: str) -> str:
-#     """
-#     Методом sub() заменяем все найденный совпадения пустой строкой
-#     и возвращаем очищенную
-#     :param input_str: строка со знаками препинания
-#     :return: строку, очищенную от этих знаков препинания
-#     """
-#     return re.sub(r'[^\w\s]', '', input_str)
-#
-#
-# test_string = 'Язык Python, явл?яясь инту"итивно понятным, пр,ост для изучения! Ну и PEP8/'
-# result = remove_punctuation(test_string)
-# print(result)
-
-# pattern = r'[^\w\s]'
-# test_string = 'яблоко,груша;банан!абрикос?слива'
-# result = re.split(pattern, test_string)
-# print(result)
-
-# pattern = r'[,:;!?.]'
-# test_string = 'яблоко,   груша;   банан!   абрикос?   слива  '
-# result = re.split(pattern, test_string)
-# print(''.join(result).split())  # -> ['яблоко', 'груша', 'банан', 'абрикос', 'слива']
-# print((list(map(lambda x: x.strip(), result))))  # через функцию map и lambda
-# print([x.strip() for x in result])  # списочное выражение
-
+pattern = r'<img[^>]+src="([^">]+)"'
+# test_string = '<img height="50" width="150" src="./images/bg.jpg">'
+html = requests.get('https://skillbox.ru').text
+# print(html)
+result = re.findall(pattern,html)
+print(result)
