e1ef75ecc94ccdc7e995f11cfceda46707cb9a20
Poltossick <1812.lev@gmail.com>
2025-07-09 10:44:55 +0300

    ReExp quantifiers

--- main.py
+++ main.py
@@ -3,61 +3,32 @@
 # r-строка — raw-string (сырая строка)
 import re
 
-# pattern = r'\b\w{4}\b' # -> регулярное выражение = \b начинает с
-# # w любого символа, состоит {4} из 4 символов и \b заканчивает
-# test_string = 'дома было холодно'
-#
-# result = re.search(pattern, test_string) # ищет только первое значение <re.Match object; span=(0, 4), match='дома'>
-# print(result) # -> <class 're.Match'>
-# result = re.findall(pattern, test_string) # ['дома', 'было']
-# print(result)
-
-# pattern = r'\d'
-# test_string = 'телефон 112'
+# pattern = r'стеклянн?ый' # вторая "н" может присутствовать, но не обязана
+# test_string = 'стекляный, стеклянный, оловянный, деревянный, серебряный'
 # result = re.findall(pattern, test_string)
-# print('Цифры есть') if result else print('Цифр нет')
+# print(result) # -> ['стекляный', 'стеклянный']
 
-# pattern = r'\d{3}'
-# test_string = 'телефон 112'
-# result = re.findall(pattern, test_string)
-# print(result)
-
-# pattern = r'начало!\Z'
-# test_string = 'Главное - начало!'
-# result = re.findall(pattern, test_string) # -> ['начало!']
-# print(result)
-
-# pattern ='[0-5][0-9]'  # -> ['12', '59']
-# test_string = 'Время - 12:59'
+# pattern = r'<img.*' # "жадный квантификатор" (greedy quantifier)
+# test_string = 'Картинка <img src="bg.jpg"> в тексте</p>' # ['<img src="bg.jpg"> в тексте</p>']
 # result = re.findall(pattern, test_string)
 # print(result)
 #
-# pattern = '[а-яА-Я]' # -> ['В', 'р', 'е', 'м', 'я']
-# test_string = 'Время - 12:59'
+# pattern = r'<img.*?>' # "ленивый квантификатор" (non-greedy, lazy quantifier)
+# test_string = 'Картинка <img src="bg.jpg"> в тексте</p>'  # ['<img src="bg.jpg">']
 # result = re.findall(pattern, test_string)
 # print(result)
 
-# pattern = '[^емя:]' # -> ['В', 'р', ' ', '-', ' ', '1', '2', '5', '9']
-# test_string = 'Время - 12:59'
+# pattern = r'<img[^>]+src="([^">]+)"' # только путь к картинке
+# test_string = 'Картинка <img src="bg.jpg"> в тексте</p>'  # ['bg.jpg']
 # result = re.findall(pattern, test_string)
 # print(result)
 
-pattern = r'\((.+?)\)' # -> ['pattern'] -- (.+?) "." - любой символ, "+?" повторяется 1 и более раз
-test_string = 'Поиск по образцу (pattern)'
+# pattern = r'<p>(.*?)</p>' # без "?" выпадет <i>и т.д.</i>
+# test_string = '<b>Вот начало: </b><p>Содержимое</p><i>и т.д.</i><p>документа</p>'  # ['Содержимое', 'документа']
+# result = re.findall(pattern, test_string)
+# print(result)
+
+pattern = r'<p[^>]*>(.*?)</p>' # содержимое сайта с атрибутами align="center"
+test_string = '<b>Центрируем</b><p align="center">Содержимое</p>'
 result = re.findall(pattern, test_string)
 print(result)
-
-pattern = r'\((.*)\)' # -> ['pattern'] -- (.*) "." - любой символ, "*" от нуля до бесконечности
-test_string = 'Поиск по образцу (pattern)'
-result = re.findall(pattern, test_string)
-print(result)
-
-# pattern = 'o{2,5}' # -> ['oo', 'ooo', 'ooooo', 'ooooo', 'oo']
-# test_string = 'Gogle1, Google2, Gooogle3, Goooooole6, Gooooooole7'
-# result = re.findall(pattern, test_string)
-# print(result)
-
-# pattern = 'Go{2,}gle' # -> ['Google', 'Gooogle']
-# test_string = 'Gogle, Google, Gooogle'
-# result = re.findall(pattern, test_string)
-# print(result)
