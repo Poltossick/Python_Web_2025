98c461a181c652d5ef7aa70dfe57cbbf7f220a5b
Poltossick <1812.lev@gmail.com>
2025-07-09 14:16:55 +0300

    OOP __init__ 'Конструктор вызван'

--- lib.py
+++ lib.py
@@ -1,3 +1,24 @@
+class Car:
+    def __init__(self, brand='N/A', model='N/A', color='N/A'):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.engine_on = False
+
+    def start_engine(self):
+        self.engine_on = True # пока не сработает
+
+    def drive_to(self, place):
+        if self.engine_on:
+            print(f'Едем в {place} на {self.brand} {self.model}')
+        else:
+            print('Заведите автомобиль')
+
+
+
+
+
+
 def summ(a, b):
     return a + b
 
--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 # ООП - объектно ориентированное программирование (encapsulation) - помещение объекта в капсулу
 # инкапсуляция - сокрытие внутренней реализации с целью сокрытия внутренних данных
-# a = 3
-# print(a.__class__.__name__)
-# СВОЙСТВА классов
-# class Fruit: # создание пустого класса
-#     pass
-# a = Fruit()  # создание экземпляра
-# b = Fruit()
-# a.name = 'Яблоко'
-# a.weight = 120
-# b.name = 'Груша'
-# b.weight = 150
-# print(f'{a.name}, {a.weight}')
-# print(f'{b.name}, {b.weight}')
 
-# МЕТОДЫ классов
-# class Greater:
-#     def hello(self, name='аноним') -> None:  # создание метода
-#         print(f'Привет, {name}!')
-#         # внутри класса отступ 1 строка
-#     def goodbye(self, name='аноним') -> None:
-#         print(f'Пока, {name}!')
-# g = Greater()
-# g.hello()
-# c = Greater()
-# c.goodbye('Виктор')
+# АНАЛИЗ предыдущих вызовов
+from lib import Car
 
+car = Car('BMW', 'X5')
+car.start_engine()
+car.drive_to('город')
+
+"""
+УНЕСТИ В LIB.PY
+class Car:
+    def __init__(self, brand='N/A', model='N/A', color='N/A'):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.engine_on = False
+
+    def start_engine(self):
+        self.engine_on = True # пока не сработает
+
+    def drive_to(self, place):
+        if self.engine_on:
+            print(f'Едем в {place} на {self.brand} {self.model}')
+        else:
+            print('Заведите автомобиль')
+"""
