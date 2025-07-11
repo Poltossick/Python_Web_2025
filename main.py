57b2df62f93081d0d00c02e57b1293a2638bf267
Poltossick <1812.lev@gmail.com>
2025-07-11 11:13:17 +0300

    NET Intro

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# ООП - (inheritance) наследование
-from lib import Zoo
+# Протоколы
 
-obj = Zoo()
-print(obj.all_animals())
-print(obj.make_all_sounds())
+# Transmission Control Protocol (TCP) — протокол управления передачей
+# Internet Protocol (IP) - разбивает на пакеты (IP-дейтаграммы)
+# TCP/IP
+# HTTP(S) - Hyper Text Transfer Protocol (Secured)
+# FTP - File Transfer Protocol
+# SMTP - Simple Mail Transfer Protocol
+# Хост-система
+# 1. Обязательная - IP-адрес: 195.34.32.11
+# 2. Необязательная - DNS (Domain Name System)
+# http(s)://www.yandex.рф//
+# ASCII %20, %2C
+# URL - Uniform Resource Locator
+
+# http(s)://domain-name.zone/page1/?parameter1=value1&parameter2=value2
