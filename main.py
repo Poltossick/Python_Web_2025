77f51f049a7a0ef1d5f4f4a311d35d8d84603a5d
Poltossick <1812.lev@gmail.com>
2025-07-11 15:31:54 +0300

    HTML + CSS

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-# Периодические задачи
+# Декоратор
 
-import schedule
-import datetime
-
-i = 1
-
-def job():
-    global i
-    print(f'Скрипт запустился {i} - раз')
-    i += 1
-    t = datetime.datetime.now()
-    print(f'Время: {t.strftime('%H:%M:%S')}')
-
-
-schedule.every(3).seconds.do(job)
-
-while True:
-    schedule.run_pending()
