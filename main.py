d04ef695191393c0ab4a5208e435a0468535cb2b
Poltossick <1812.lev@gmail.com>
2025-07-14 14:08:19 +0300

    csv intro

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-# Декоратор
+# CSV-файлы
 
+import csv
+from os import write
+
+data = [
+    ['name', 'age', 'city'],
+    ['Petr', '28', 'Moscow'],
+    ['Boris', '57', 'Magadan'],
+    ['Oleg', '42', 'Abakan'],
+    ['Ivan', '22', 'Omsk']
+]
+
+with open('people.csv', 'r', encoding='utf-8') as f1:
+    reader = csv.reader(f1, delimiter=',', quotechar='"')
+    for row in reader:
+        print(row)
+
+with open('employee.csv', 'w', newline='', encoding='utf-8') as f2:
+    writer = csv.writer(f2)
+    writer.writerows(data)
