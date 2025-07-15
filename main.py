0c67bb728a1b87063810ea6a17d5f4fd9c6f3ebe
Poltossick <1812.lev@gmail.com>
2025-07-15 15:44:54 +0300

    Decorator INTRO

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 # Декораторы
 
-def answer(question):
-    return 'Думайте сами'
 
-def dialog():
-    def answer(question):
-        if question.lower().startswith('когда'):
-            return 'Никогда'
-        else:
-            return 'Упс'
-    question = input()
-    while question != '':
-        print(answer(question))
-        question = input()
+def upper_case_print(old_function):
+    def new_function(*args, **kwargs):
+        args_up_case = [str(arg).upper() for arg in args]
+        old_function(*args_up_case, **kwargs)
+    return new_function
 
-dialog()
+new_print = upper_case_print(print)
+new_print('привет андрей')
+
+
+# def answer(question):
+#     return 'Думайте сами'
+#
+# def dialog():
+#     def answer(question):
+#         if question.lower().startswith('когда'):
+#             return 'Никогда'
+#         else:
+#             return 'Упс'
+#     question = input()
+#     while question != '':
+#         print(answer(question))
+#         question = input()
+#
+# dialog()
