2045e88906840a5c383a894ac8c675f5abdd9cff
Poltossick <1812.lev@gmail.com>
2025-07-15 14:15:41 +0300

    sql CRUD

--- main.py
+++ main.py
@@ -9,21 +9,44 @@
 import sqlite3
 import csv
 
-connection = sqlite3.connect('./database/movies.sqlite')
-cursor = connection.cursor()
-with open ('people.csv', 'rt', encoding='utf-8') as f1:
-    reader = csv.reader(f1, delimiter=',')
-    next(reader)
-    for name, age in reader:
-        cursor.execute(
-            """
-            insert into
-            users(name, age)
-            values(?, ?)
-            """,(name, int(age))
+class Crud:
+    def __init__(self, db_path):
+        self._connection = sqlite3.connect(db_path)
+        self._cursor = self._connection.cursor()
+
+    def create(self, table_name, name, age):
+        result = self._cursor.execute(
+            f'insert into {table_name}(name, age) values(?, ?)',(name,int(age))
+            )
+        self._connection.commit()
+
+    def read(self, table_name):
+        result = self._cursor.execute(
+            f'select * from {table_name}'
+        ).fetchall()
+
+        for num, name, age in result:
+            print(num, name, age)
+
+    def update(self, table_name, id_num, name=None, age=None):
+        result = self._cursor.execute(
+            f'update {table_name} set name="{name}", age={age} where id={id_num}'
+            )
+        self._connection.commit()
+
+    def delete(self, in_mun, table_name):
+        result = self._cursor.execute(
+            f'delete from {table_name} where id = {in_mun}'
         )
+        self._connection.commit()
+
+    def __del__(self):
+        self._cursor.close()
+        self._connection.close()
 
 
-
-connection.commit()
-connection.close()
+db = Crud('./database/movies.sqlite')
+db.delete(24, 'users')
+db.create('users', 'Иван', '87')
+db.update('users', 23, 'Николай', 72)
+db.read('users')
