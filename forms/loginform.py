dac810980d68e2af9d6972df54c4cd58e9a5f413
Poltossick <1812.lev@gmail.com>
2025-07-17 15:15:10 +0300

    Form External Version

--- forms/loginform.py
+++ forms/loginform.py
@@ -4,7 +4,7 @@ from wtforms import BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
+    username = StringField('Логин', validators=[DataRequired('Обязательно для заполнения')])
+    password = PasswordField('Пароль', validators=[DataRequired('Без пароля не пройдешь')])
     remember_me = BooleanField('Запомнить данные')
     submit = SubmitField('Войти')
