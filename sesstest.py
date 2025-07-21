e528708a03cd2755e2e64ab9c8f4b99d19864db7
Poltossick <1812.lev@gmail.com>
2025-07-21 12:15:43 +0300

    session and lifetime

--- sesstest.py
+++ sesstest.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, make_response, redirect
+import datetime
+
+from flask import Flask, request, make_response, redirect, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'just_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(hours=5)
 
 @app.route('/')
 def index():
@@ -17,6 +21,14 @@ def cookie_test():
         res.set_cookie('visit_count', '1', max_age=60*60*24*30)
     return res
 
+@app.route('/session-test')
+def session_test():
+    visit_count = int(session.get('visit_count', 0))
+    session['visit_count'] = visit_count + 1
+    # session.pop('visit_count', None) принудительное удалить сессию с этим ключом
+    return make_response(
+        f'Вы на этой странице {visit_count + 1} раз(а).'
+    )
 
 
 if __name__ == '__main__':
