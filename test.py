1888ac1238d89df0ad1d4b31c9d081fcb91a3b3d
Poltossick <1812.lev@gmail.com>
2025-07-22 11:45:49 +0300

    from requests import get, post, put, delete

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-from requests import get
+from requests import get, post, put, delete
 
 # print(get('http://localhost:5000/api/news').json())
-print(get('http://localhost:5000/api/news/1').json())
-print(get('http://localhost:5000/api/news/1000').json())
-print(get('http://localhost:5000/api/news/q').json())
+# print(get('http://localhost:5000/api/news/1').json())
+# print(get('http://localhost:5000/api/news/1000').json())
+# print(get('http://localhost:5000/api/news/q').json())
+
+# print(post('http://localhost:5000/api/news', json={}).json())
+# print(post('http://localhost:5000/api/news', json={'title': 'проверка заголовка'}).json())
+# print(post('http://localhost:5000/api/news', json={'title': 'проверка заголовка',
+#                                                    'content': 'содержание новости',
+#                                                    'user_id': 3, 'is_private': False}).json())
+# print(post('http://localhost:5000/api/news', json={'title': 'заголовок',
+#                                                    'content': 'текст новости',
+#                                                    'user_id': 2, 'is_private': False}).json())
+
+print(delete('http://localhost:5000/api/news/500').json())
